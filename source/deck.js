87f365709984b58cfb0fd169918ae18b3cca0941
wangharold001 <wangharold001@gmail.com>
2025-04-19 15:38:28 -0700

    deck logic + testing

---
+++ source/deck.js
@@ -0,0 +1,27 @@
+export const SUITS = ['Spade', 'Heart', 'Diamond', 'Club'];
+
+export const NUMS = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+
+// Builds deck in set suit & rank order
+export function buildDeck() {
+  const deck = [];
+  for (const suit of SUITS) {
+    for (const rank of NUMS) deck.push({ rank, suit });
+  }
+  return deck;
+}
+
+//for testing shuffle function
+export function cardToString(card) {
+    return `${card.rank}' of '${card.suit}s`;
+  }
+
+//When getting new shuffled deck, need to call shuffle on buildDeck()
+export function shuffle(deck, { inPlace = true, rng = Math.random } = {}) {
+  const d = deck
+  for (let i = d.length - 1; i > 0; i--) {
+    const j = Math.floor(rng() * (i + 1));
+    [d[i], d[j]] = [d[j], d[i]];
+  }
+  return d;
+}
