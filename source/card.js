42d0b900cf84bb0f2971fff81f46fca4bdd9ec57
nkcha02 <nkchang@ucsd.edu>
2025-04-20 14:27:54 -0700

    n

--- source/card.js
+++ source/card.js
@@ -20,6 +20,13 @@ class PlayingCard extends HTMLElement {
     const backDiv = this.shadowRoot.querySelector(".back");
     backDiv.innerHTML = '<span style="border:1px black; border-style:solid">◆</span>';
   }
-}
+
+  renderFront() {
+    const frontDiv = this.shadowRoot.querySelector(".front");
+    const content = this.innerHTML.trim();
+    frontDiv.innerHTML = content
+    ? content : '<span style="border:1px solid black; padding: 4px;">?</span>';
+  }
+  }
 
 customElements.define('playing-card', PlayingCard);
